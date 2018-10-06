3f8023296605bf4361af1644fbead8f03ed441fa
진명 <@172.17.154.161>
2018-10-06 15:32:31 +0900

    #4 modify testMod() in CalculatorTest.java

--- src/test/java/edu/handong/csee/pp1/bigdata/lab1_1/CalculatorTest.java
+++ src/test/java/edu/handong/csee/pp1/bigdata/lab1_1/CalculatorTest.java
@@ -29,6 +29,8 @@ public class CalculatorTest {
 	@Test
 	public void testMod() {
 		// TODO Complete this test case
+		Calculator myCalc = new Calculator();
+		assertEquals(myCalc.mod(7,3),1);
 	}
 
 	@Test
